c718fd3d66b07e57216e9bc7cb6c55cc7ffcf853
c-cal <github-2c7c@zebrina.fr>
2019-12-07 00:16:58 +0100

    style: refactor

--- src/components/structures/WatchaAdmin.js
+++ src/components/structures/WatchaAdmin.js
@@ -1,12 +1,11 @@
-import React from "react";
+import { Component } from "react";
 import PropTypes from "prop-types";
 import { MatrixClient } from "matrix-js-sdk";
 import * as languageHandler from "../../languageHandler";
 
-class WatchaAdmin extends React.Component {
+class WatchaAdmin extends Component {
     constructor(props) {
         super(props);
-        this.openWatchaAdmin = this.openWatchaAdmin.bind(this);
         this.state = { isServerAdmin: false };
     }
 
@@ -29,7 +28,7 @@ class WatchaAdmin extends React.Component {
             });
     }
 
-    openWatchaAdmin(ev) {
+    openWatchaAdmin = ev => {
         // the token will be retrieved in watcha-admin.git/src/App.js
         const key = Math.random()
             .toString(36)
@@ -41,50 +40,71 @@ class WatchaAdmin extends React.Component {
             this.context.matrixClient.getAccessToken();
         localStorage.setItem("watcha-" + key, value);
         window.open("/admin?key=" + key, "_blank");
-    }
+    };
 
     render() {
-        if (!this.props.collapsed) {
-            return this.state.isServerAdmin ? (
-                <div className="mx_WatchaAdminContainer">
-                    <button
-                        type="button"
-                        className="mx_WatchaAdminButton"
-                        onClick={this.openWatchaAdmin}
-                        aria-label="Open Watcha administration"
-                        role="button"
-                    >
-                        Administration
-                        <img
-                            id="mx_WatchaAdminIcon"
-                            src={require("../../../res/img/watcha_admin.svg")}
-                            alt="admin"
-                            width="15"
-                            height="15"
-                        />
-                    </button>
-                </div>
-            ) : (
-                <div>TEST</div>
-            );
-        } else {
-            return (
-                <div
-                    className="mx_WatchaAdminContainer"
+        if (!this.state.isServerAdmin) {
+            return <div>TEST</div>;
+        }
+        return this.props.collapsed ? (
+            <CollapsedAdminAccess />
+        ) : (
+            <AdminAccess />
+        );
+    }
+}
+
+class AdminAccess extends Component {
+    static contextTypes = {
+        matrixClient: PropTypes.instanceOf(MatrixClient)
+    };
+
+    render() {
+        return (
+            <div className="mx_WatchaAdminContainer">
+                <button
+                    type="button"
+                    className="mx_WatchaAdminButton"
+                    onClick={this.openWatchaAdmin}
                     aria-label="Open Watcha administration"
                     role="button"
                 >
+                    Administration
                     <img
-                        id="mx_WatchaAdminIcon_collapsed"
+                        id="mx_WatchaAdminIcon"
                         src={require("../../../res/img/watcha_admin.svg")}
-                        onClick={this.openWatchaAdmin}
                         alt="admin"
-                        width="25"
-                        height="25"
+                        width="15"
+                        height="15"
                     />
-                </div>
-            );
-        }
+                </button>
+            </div>
+        );
+    }
+}
+
+class CollapsedAdminAccess extends Component {
+    static contextTypes = {
+        matrixClient: PropTypes.instanceOf(MatrixClient)
+    };
+
+    render() {
+        return (
+            <div
+                className="mx_WatchaAdminContainer"
+                aria-label="Open Watcha administration"
+                role="button"
+            >
+                <img
+                    id="mx_WatchaAdminIcon_collapsed"
+                    src={require("../../../res/img/watcha_admin.svg")}
+                    onClick={this.openWatchaAdmin}
+                    alt="admin"
+                    width="25"
+                    height="25"
+                />
+            </div>
+        );
     }
 }
 
