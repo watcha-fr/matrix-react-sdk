f79e0d8326928958798cbecf946eedfa5165bdaf
c-cal <github-2c7c@zebrina.fr>
2022-07-22 11:56:22 +0200

    fix: replace thread help link with watcha tuto

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -268,7 +268,10 @@ export const SETTINGS: {[setting: string]: ISetting} = {
             caption: () => <>
                 <p>{ _t("Keep discussions organised with threads.") }</p>
                 <p>{ _t("Threads help keep conversations on-topic and easy to track. <a>Learn more</a>.", {}, {
+                    /* watcha!
                     a: (sub) => <a href="https://element.io/help#threads" rel="noreferrer noopener" target="_blank">
+                    !watcha */
+                    a: (sub) => <a href="https://youtu.be/2iI0rQ5b8Hk" rel="noreferrer noopener" target="_blank"> { /* watcha+ */ }
                         { sub }
                     </a>,
                 }) }</p>
