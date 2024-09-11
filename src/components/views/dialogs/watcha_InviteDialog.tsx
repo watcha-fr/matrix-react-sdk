b119870b0590398ee138157235bbb53e448231b4
dlamarcheteamnet <d.lamarche@teamnet.fr>
2024-09-11 14:48:51 +0200

    test partner no dm

--- src/components/views/dialogs/watcha_InviteDialog.tsx
+++ src/components/views/dialogs/watcha_InviteDialog.tsx
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+import { logger } from "matrix-js-sdk/src/logger";
 import React, { createRef } from "react";
 import { debounce } from "lodash";
 import { IInvite3PID } from "matrix-js-sdk/src/@types/requests";
