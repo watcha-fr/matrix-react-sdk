5e1bfd645c0907c3dfbcc3587aae3ea029a6804a
dlamarcheteamnet <d.lamarche@teamnet.fr>
2024-10-30 15:25:38 +0100

    Hide agenda et tâche

--- src/components/views/dialogs/RoomSettingsDialog.tsx
+++ src/components/views/dialogs/RoomSettingsDialog.tsx
@@ -190,7 +190,7 @@ class RoomSettingsDialog extends React.Component<IProps, IState> {
             }
             const client = MatrixClientPeg.get();
             const room = client?.getRoom(this.state.room.roomId);
-            if(client!.getUserId() !== null){
+            /*if(client!.getUserId() !== null){
                 const canSetCalendar = room?.currentState.maySendStateEvent(CALENDAR_EVENT_TYPE, client!.getUserId()!);
                 if (canSetCalendar) {
                     tabs.push(
@@ -202,7 +202,7 @@ class RoomSettingsDialog extends React.Component<IProps, IState> {
                         ),
                     );
                 }
-            }
+            }*/
         }
         // +watcha
         tabs.push(
