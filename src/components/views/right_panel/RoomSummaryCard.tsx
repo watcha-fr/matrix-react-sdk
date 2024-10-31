ee9f019c61ce41f38acad0f62d8eccb8ae546e1c
dlamarcheteamnet <d.lamarche@teamnet.fr>
2024-10-31 16:01:57 +0100

    Hide invite button for partners

--- src/components/views/right_panel/RoomSummaryCard.tsx
+++ src/components/views/right_panel/RoomSummaryCard.tsx
@@ -568,13 +568,12 @@ const RoomSummaryCard: React.FC<IProps> = ({ room, permalinkCreator, onClose, on
                 // XXX: https://github.com/element-hq/compound/issues/288
                 onSelect={() => {}}
             />
-            {!cli.isPartner && /* watcha+ */
-                <MenuItem
-                Icon={UserAddIcon}
-                label={_t("action|invite")}
-                disabled={!canInviteToState}
-                onSelect={() => inviteToRoom(room)}
-            />}
+            <MenuItem
+            Icon={UserAddIcon}
+            label={_t("action|invite")}
+            disabled={!canInviteToState}
+            onSelect={() => inviteToRoom(room)}
+            />
             {showShareRoomButton && <MenuItem Icon={LinkIcon} label={_t("watcha|share_room")} onSelect={onShareRoomClick} /> /* watcha+ */}
             <MenuItem Icon={SettingsIcon} label={_t("common|settings")} onSelect={onRoomSettingsClick} />
 
