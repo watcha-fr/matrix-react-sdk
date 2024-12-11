5a74bcaa1d9b46602342d25f1a3d2b6f5e4712bb
dlamarcheteamnet <d.lamarche@teamnet.fr>
2024-12-11 15:23:52 +0100

    show 2 videos call button and change password partner Sitiv Config

--- src/components/views/rooms/RoomHeader.tsx
+++ src/components/views/rooms/RoomHeader.tsx
@@ -26,7 +26,7 @@ import { Icon as ErrorIcon } from "@vector-im/compound-design-tokens/icons/error
 import { Icon as PublicIcon } from "@vector-im/compound-design-tokens/icons/public.svg";
 import { EventType, JoinRule, type Room } from "matrix-js-sdk/src/matrix";
 import { ViewRoomOpts } from "@matrix-org/react-sdk-module-api/lib/lifecycles/RoomViewLifecycle";
-import SettingsStore from "../../../settings/SettingsStore"; // watcha+
+//import SettingsStore from "../../../settings/SettingsStore"; // watcha+
 import { useRoomName } from "../../../hooks/useRoomName";
 import { RightPanelPhases } from "../../../stores/right-panel/RightPanelStorePhases";
 import { useTopic } from "../../../hooks/room/useTopic";
@@ -55,7 +55,7 @@ import { RoomKnocksBar } from "./RoomKnocksBar";
 import { isVideoRoom } from "../../../utils/video-rooms";
 import { notificationLevelToIndicator } from "../../../utils/notifications";
 import { CallGuestLinkButton } from "./RoomHeader/CallGuestLinkButton";
-import { UIFeature } from "../../../settings/UIFeature"; // watcha+
+//import { UIFeature } from "../../../settings/UIFeature"; // watcha+
 
 export default function RoomHeader({
     room,
